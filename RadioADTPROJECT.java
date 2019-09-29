7654994aca25b4a9cb4597bd4cde1689d1099c9b
Brando0o <55958066+Brando0o@users.noreply.github.com>
2019-09-29 14:41:07 -0400

    Add files via upload

--- RadioADTPROJECT.java
+++ RadioADTPROJECT.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -14,8 +15,10 @@ public class RadioADTPROJECT {
     
     public static void main(String[] args)
     {
-    
-        
+    RadioScanner station = new RadioScanner();
+    station.setStation(870);
+    station.loadAMStations();
+    station.showAmStations();
 
 
 
